70156cbae9f57c0717404f73d929065a597b166d
Miriana Falcone <miriana.falcone7@gmail.com>
2023-11-16 13:11:43 +0100

    node 1

---
+++ esercizio1.js
@@ -0,0 +1,8 @@
+/* Exercise 1: Create a sum script
+Create a script that outputs the total of 5 + 6 when you run it with Node.js. */
+
+const sum = (n1, n2) => {
+  return n2 + n2;
+};
+
+console.log(sum(5, 6));
