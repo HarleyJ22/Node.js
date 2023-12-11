adaa83a30b67af08076bf0d9af1d36410fbe4fc0
Miriana Falcone <miriana.falcone7@gmail.com>
2023-12-11 20:10:15 +0100

    JWT passport

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,29 +1,6 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import pgPromise from "pg-promise";
-
-const db = pgPromise()("postgres://postgres:admin@localhost:5432/postgres");
-
-const setupDb = async () => {
-  await db.none("DROP TABLE IF EXISTS planets");
-  await db.none(`
-    CREATE TABLE planets(
-      id SERIAL NOT NULL PRIMARY KEY,
-      name TEXT NOT NULL
-      image TEXT
-    )
-  `);
-
-  await db.none(`INSERT INTO planets (name) VALUES ('Earth')`);
-  await db.none(`INSERT INTO planets (name) VALUES ('Mars')`);
-  await db.none(`INSERT INTO planets (name) VALUES ('Juptier')`);
-
-  const planets = await db.many(`SELECT * FROM planets`);
-
-  console.log(planets);
-};
-
-setupDb();
+import { db } from "../db.js";
 
 // JOI VALIDATION
 const planetSchema = Joi.object({
